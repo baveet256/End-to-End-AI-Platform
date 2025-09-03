0d4d6d5ddd42cd76bd636a57b519073fa108275f
Baveet Hora <bhora@asu.edu>
2025-09-03 03:30:12 -0700

    navbar, mobile drawer done

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,4 +1,5 @@
 import { SidebarProvider } from "@/components/ui/sidebar";
+import { DashboardNavbar } from "@/modules/dashboard/ui/components/dashboard-navbar";
 import { DashboardSidebar } from "@/modules/dashboard/ui/components/dashboard-sidebar";
 import { Children } from "react";
 
@@ -12,7 +13,10 @@ const Layout = ({children}: Props) => {
 
         <DashboardSidebar/>
         
-        <main className="flex flex-col h-screen w-screen bg-muted">{children}</main>
+        <main className="flex flex-col h-screen w-screen bg-muted">
+            <DashboardNavbar/>
+            
+            {children}</main>
         
         
         </SidebarProvider> );
